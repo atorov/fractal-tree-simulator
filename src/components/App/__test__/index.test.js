d5f84db7f8e485d2324312bac6b05867c3e241d5
Veselin <veselin.lk.petrov@gmail.com>
2018-02-06 16:15:19 +0200

    fixed: unit testing: "window.p5 is not a constructor" issue

--- src/components/App/__test__/index.test.js
+++ src/components/App/__test__/index.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from '../';
 
+// window.p5 = function() {};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
